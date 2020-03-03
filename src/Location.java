6e31d433e787a95163fde09cd47571152f9a8105
Ayoub Elmohamedi <ayoubprog61@gmail.com>
2020-03-03 22:29:12 +0100

    reformat code

--- src/Location.java
+++ src/Location.java
@@ -2,19 +2,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Location {
-	
-	private Map<String,Integer> exits ;
-	private String locationDetail ;
+
+	private Map<String, Integer> exits;
+	private String locationDetail;
 	private int locationId;
-	
-	public Location(int locationId,String description) {
+
+	public Location(int locationId, String description) {
 		this.locationDetail = description;
 		this.locationId = locationId;
 		exits = new HashMap<String, Integer>();
 		this.exits.put("Q", 0);
 	}
-	
-	public void addExit(String direction,int locationId) {
+
+	public void addExit(String direction, int locationId) {
 		exits.put(direction, locationId);
 	}
 
@@ -29,10 +29,5 @@ public class Location {
 	public int getLocationId() {
 		return locationId;
 	}
-	
-	
-	
-	
-	
 
 }
